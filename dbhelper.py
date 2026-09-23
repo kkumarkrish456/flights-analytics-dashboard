3624af6340f781ab581f7d724c0132daf9c48273
Krish Kumar <kkumarkrish.456@gmail.com>
2026-09-23 22:39:08 +0530

    Remove database credentials

--- dbhelper.py
+++ dbhelper.py
@@ -1,15 +1,16 @@
 import psycopg2
+import os
 import pandas as pd
 
 class DB:
     def __init__(self):
         try:
             self.conn = psycopg2.connect(
-                host = "aws-0-ap-southeast-1.pooler.supabase.com",
-                database = "postgres",
-                user = "postgres.guckddiwnswmyjyjxxep",
-                password = "@Iamkrish456",
-                port = "5432"
+                host=os.getenv("DB_HOST"),
+                database=os.getenv("DB_NAME"),
+                user=os.getenv("DB_USER"),
+                password=os.getenv("DB_PASSWORD"),
+                port=os.getenv("DB_PORT")
             )
             self.cursor = self.conn.cursor()
         except Exception as Error:
